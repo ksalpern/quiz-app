053c796fc1a3b4af7907c5c8d7beb011689980ca
Kseniia <ksenia.pidopryhora@gmail.com>
2025-09-05 20:44:55 +0300

    Add initial project setup with Next.js, ESLint, Prettier, and Husky configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+    },
+  }),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
+
+export default eslintConfig;
